4b7bae14dccf730838ad561b9eba83952ba6e2a3
Colin Stryker <cstryker@newrelic.com>
2021-10-05 18:34:32 -0700

    support direct fetch of code errors for user

--- api_server/modules/code_errors/indexes.js
+++ api_server/modules/code_errors/indexes.js
@@ -3,10 +3,7 @@
 'use strict';
 
 module.exports = {
-	byTeamId: {
-		teamId: 1,
-		createdAt: -1
-	},
+
 	byObjectId: {
 		objectId: 1,
 		objectType: 1
@@ -17,7 +14,6 @@ module.exports = {
 	// BUT WITHOUT SIGNIFICANT STRUCTURAL CHANGES TO OUR
 	// CODEBASE CANNOT BE DIRECTLY READ FROM THIS FILE
 	byLastActivityAt: {
-		teamId: 1,
 		lastActivityAt: -1
 	},
 	
